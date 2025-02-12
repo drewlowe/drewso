4321be7d61786a2d61ca90af514ea3d44bd93f16
Drew Lowe <drew@drew.so>
2025-02-12 01:07:00 -0500

    basic mdx configuration - needs work

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import createMDX from '@next/mdx'
 
 const nextConfig: NextConfig = {
-
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+
+})
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
