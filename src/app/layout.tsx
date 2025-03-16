da4e4d122838a629af9b26efbda87e61a52b0ee6
Drew Lowe <drew@drew.so>
2025-03-15 23:33:41 -0400

    wrapped layout in mdx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,15 +59,17 @@ export default function RootLayout({
         >
           <div className="flex min-h-screen justify-center bg-[var(--background)] text-[var(--foreground)]">
             <div className="max-w-[617px] w-full py-2 px-5 md:p-2">
-              <Header />
-              {children}
-              <Footer />
-              <Analytics />
-              <SpeedInsights />
+              <Header/>
+              <div className={'prose prose-h1:text-2xl dark:prose-invert mt-7'}>
+                {children}
+              </div>
+              <Footer/>
+              <Analytics/>
+              <SpeedInsights/>
+              </div>
             </div>
-          </div>
-        </ThemeProvider>
-      </body>
+    </ThemeProvider>
+    </body>
     </html>
-  );
+);
 }
