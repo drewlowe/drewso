6566e7e2b36174c4515dce7a51adeca6b8ff1e44
Drew Lowe <drew@drew.so>
2025-07-11 17:56:11 -0400

    api logos

--- src/components/tech-logo/ServiceLogoGrids.tsx
+++ src/components/tech-logo/ServiceLogoGrids.tsx
@@ -16,3 +16,20 @@ export const FullStackLogos = () => (
     <TechLogo path={"/wasm.png"} />
   </div>
 );
+
+export const APILogos = () => (
+  <div className="w-full md:w-1/2 grid grid-cols-4 gap-x-8 gap-y-8 items-center">
+    <TechLogo path={"/openapi.png"} />
+    <TechLogo path={"/graphql.png"} />
+    <TechLogo path={"/nest.png"} />
+    <TechLogo path={"/django.png"} />
+    <TechLogo path={"/aws.png"} />
+    <TechLogo path={"/gcp.png"} />
+    <TechLogo path={"/webhooks.png"} />
+    <TechLogo path={"/websocket.png"} />
+    <TechLogo path={"/redis.png"} />
+    <TechLogo path={"/postgres.png"} />
+    <TechLogo path={"/mysql.png"} />
+    <TechLogo path={"/firebase.png"} />
+  </div>
+);
--- src/components/tech-logo/techLogos.ts
+++ src/components/tech-logo/techLogos.ts
@@ -65,4 +65,64 @@ export const techLogos = {
     width: 512,
     height: 512,
   },
+  "/aws.png": {
+    alt: "Amazon Web Services",
+    width: 1200,
+    height: 719,
+  },
+  "/firebase.png": {
+    alt: "Firebase",
+    width: 364,
+    height: 364,
+  },
+  "/gcp.png": {
+    alt: "Google Cloud Platform",
+    width: 360,
+    height: 360,
+  },
+  "/graphql.png": {
+    alt: "GraphQL",
+    width: 512,
+    height: 512,
+  },
+  "/nest.png": {
+    alt: "Nest",
+    width: 200,
+    height: 200,
+  },
+  "/openapi.png": {
+    alt: "OpenAPI",
+    width: 320,
+    height: 320,
+  },
+  "/django.png": {
+    alt: "django",
+    width: 2560,
+    height: 887,
+  },
+  "/redis.png": {
+    alt: "Redis",
+    width: 256,
+    height: 256,
+  },
+  "/postgres.png": {
+    alt: "PostgreSQL",
+    width: 1200,
+    height: 1237,
+  },
+  "/mysql.png": {
+    alt: "MySQL",
+    width: 512,
+    height: 512,
+  },
+  "/webhooks.png": {
+    alt: "Webhooks",
+    width: 240,
+    height: 240,
+  },
+  "/websocket.png": {
+    alt: "Websocket",
+    width: 512,
+    height: 512,
+  },
 } as const satisfies Record<string, ITechLogo>;
