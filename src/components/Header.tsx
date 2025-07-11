c1a1ff4bbd41eb60bd60c9fd50e3ffd00c618c3b
Drew Lowe <drew@drew.so>
2025-07-10 23:28:58 -0400

    quick updates

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,17 @@
 import { JetBrains_Mono } from 'next/font/google'
-import { ThemeToggle } from "@/components/ThemeToggle"
 import Link from 'next/link'
+
+import { ThemeToggle } from "@/components/ThemeToggle"
+import { Logo } from './Logo'
 
 const jetBrainsMono = JetBrains_Mono({ subsets: ['latin'] })
 
 export default function Header() {
     return (
         <div className="w-full mt-4 mb-5 md:mb-9 flex flex-row items-center justify-between">
-            <h2 className='font-bold text-lg'>
-                <Link href="/">Drew Lowe</Link>
-            </h2>
+            <div className='w-[100px]'>
+                <Link href="/"><Logo /></Link>
+            </div>
             <div className={`flex flex-row text-sm font-light items-center gap-4 ${jetBrainsMono.className}`}>
                 <Link href="/tech-stack">Stack</Link>
                 <Link href="/bio">Bio</Link>
